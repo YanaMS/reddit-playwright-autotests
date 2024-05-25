e3b1c7b42e21a08fed64c5e21d6ad024c5e5011d
Yana <yana.sukharieva@gmail.com>
2024-05-25 22:50:58 +0200

    locators

--- tests/where-to-buy-page.spec.ts
+++ tests/where-to-buy-page.spec.ts
@@ -5,10 +5,18 @@ test('Page "Where to Buy"', async ({ page }) => {
   // Navigate to the main page
   await page.goto('');
 
-  await page.getByRole('button', { name: 'Де купити' }).first().click();
-  
-  const mainSection = page.locator('#site-main');
-  await expect(mainSection).toHaveText(/Хто ви?/); 
-  const partnerButton = page.getByRole('button', { name: 'Стати партнером' });
-  await expect(partnerButton).toBeVisible();
+
+  //Search for gaming subReddit
+let l1 = page.locator("reddit-search-large");
+let tt = await l1.textContent();
+console.log(tt);
+await l1.click();
+await l1.locator("input").fill("gaming");
+let t2 = await l1.locator("[class='text-neutral-content']").all();
+for (const element of t2)
+  console.log(await element.innerText());
+  //Open subReddit
+
+  //
+console.log("End");  
 });
